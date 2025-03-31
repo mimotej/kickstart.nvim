74534ce10f7fecd20d2f4a17448aa4b1a1e49310
Michal Drla <michal.drla@outlook.com>
2025-03-31 21:28:55 +0200

    feat: Add refactoring
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

--- init.lua
+++ init.lua
@@ -166,6 +166,18 @@ vim.opt.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+-- Refactoring keymaps
+vim.keymap.set('x', '<leader>re', ':Refactor extract ')
+vim.keymap.set('x', '<leader>rf', ':Refactor extract_to_file ')
+
+vim.keymap.set('x', '<leader>rv', ':Refactor extract_var ')
+
+vim.keymap.set({ 'n', 'x' }, '<leader>ri', ':Refactor inline_var')
+
+vim.keymap.set('n', '<leader>rI', ':Refactor inline_func')
+
+vim.keymap.set('n', '<leader>rb', ':Refactor extract_block')
+vim.keymap.set('n', '<leader>rbf', ':Refactor extract_block_to_file')
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
