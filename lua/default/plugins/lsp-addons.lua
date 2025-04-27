adf463f823f518b97fef5f1cad2bf288ca37a22c
Michal Drla <michal.drla@protonmail.com>
2025-04-27 17:15:47 +0200

    feat: Add better hover, actions and lsp signs

---
+++ lua/default/plugins/lsp-addons.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'kosayoda/nvim-lightbulb',
+    config = function()
+      require('nvim-lightbulb').setup {
+        sign = { enabled = true, priority = 10 },
+        float = { enabled = false },
+        virtual_text = { enabled = false },
+        status_text = { enabled = true, text = '💡' },
+        autocmd = { enabled = true },
+      }
+    end,
+  },
+  { 'folke/lsp-colors.nvim', lazy = true },
+  { 'folke/trouble.nvim', lazy = true },
+}
