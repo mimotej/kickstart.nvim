aa58af33ae57169b59f8020b23462330816242c9
Michal Drla <michal.drla@outlook.com>
2025-04-27 21:24:47 +0200

    feat: Enable code actions from typescript server and disable autoformat on typescript
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

--- lua/default/plugins/typescript-tools.lua
+++ lua/default/plugins/typescript-tools.lua
@@ -2,6 +2,6 @@ return {
   {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
+    opts = { settings = { expose_as_code_action = 'all' } },
   },
 }
